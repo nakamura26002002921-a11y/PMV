0b843126add3c523f6a6648cc69695a2bd142073
nakamura26002002921-a11y <nakamura26002002921@gmail.com>
2026-04-20 09:51:39 +0900

    Update monte.py

--- python/monte.py
+++ python/monte.py
@@ -16,11 +16,11 @@ def monte_volume(protein, waters, r, max_iter=10, n_points=1000):
     waters = waters.positions
     radius_dict = {'C':1.7,'H':1.2,'O':1.52,'N':1.55,'S':1.8}
     radiuses = np.array([radius_dict[e] for e in protein.elements])
-    number = pmv_calc.func1(atoms, radiuses, waters, r)
+    number = pmv_calc.func1(atoms, waters, radiuses, r)
     times, results = [], []
     for _ in range(max_iter):
         start = time.perf_counter()
-        result = pmv_calc.func2(atoms, radiuses, r, n_points)
+        result = pmv_calc.func2(atoms, radiuses, np.arange(len(atoms), dtype=np.int32), r, n_points)
         end = time.perf_counter()
         times.append(end - start)
         results.append(result)
@@ -34,8 +34,8 @@ def main():
     with open("result.txt", "w") as f:
         for ts, _ in enumerate(u.trajectory[:10000]):
             print(ts)
-            t, vol, n = monte_volume(protein_heavy, waters, radius=1.4)
-            f.write(f"{ts}\t{t}\t{vol}\t{n}\n")
+            t, vol, n, num = monte_volume(protein_heavy, waters, r=1.4)
+            f.write(f"{ts}\t{t}\t{vol}\t{n}\t{num}\n")
 
 if __name__ == "__main__":
     main()
